bf5522f55a74220bc72b3db95ccd23a1760fdaec
avbds002 <avbds2077@gmail.com>
2024-06-29 19:43:20 -0300

    Lesson F744 LEFT JOIN

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2990;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2990Application implements CommandLineRunner {
@@ -19,6 +24,12 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		List<EmpregadoDeptProjection> list = repository.search1();
+		List<EmpregadoDeptDTO> result1 = list.stream().map(x -> new EmpregadoDeptDTO(x)).collect(Collectors.toList());
+
+		for (EmpregadoDeptDTO empregadoDeptDTO : result1) {
+			System.out.println(empregadoDeptDTO);
+		}
+		System.out.println("\n\n\n");
 	}
 }
